c93dcfccd769f38c8f588447c4109002749ba7b7
he <heqingtong@foxmail.com>
2021-01-21 11:15:15 +0800

    1

--- src/views/pages/statisticsOut.vue
+++ src/views/pages/statisticsOut.vue
@@ -1,131 +1,128 @@
 <template>
-    <div class="app-page">
+  <div class="app-page">
     <div class="el-search">
       <div class="prepend">出库统计</div>
     </div>
-      <div class="app-page-select">
-        <el-form :inline="true">
-          <el-form-item class="el-form-item" label="物品名称：">
-            <el-input
-              maxlength="30"
-              v-model="searchFormData.searchName"
-              class="input-with-select"
-            ></el-input>
-          </el-form-item>
-          <el-form-item class="el-form-item" label="仓库类型：">
-            <el-select
-              v-model="searchFormData.searchStoreType"
-              placeholder="选择仓库类型"
-              @change="searchStoreTypeChange($event)"
-            >
-              <el-option
-                v-for="item in storeTypeList"
-                :key="item.id"
-                :label="item.name"
-                :value="item.id"
-              ></el-option>
-            </el-select>
-          </el-form-item> 
-          <el-form-item class="el-form-item" label="仓库名称：">
-            <el-select
-              v-model="searchFormData.searchStore"
-              placeholder="选择仓库名称"
-            >
-              <el-option label="全部仓库" value=""></el-option>
-              <el-option
-                v-for="item in storeList"
-                :key="item.id"
-                :label="item.name"
-                :value="item.id"
-              ></el-option>
-            </el-select>
-          </el-form-item>
-          <el-form-item class="el-form-item" label="出库时间：">
-            <el-date-picker
-              v-model="searchFormData.serachTime"
-              type="date"
-              placeholder="选择日期"
-            >
-            </el-date-picker>
-          </el-form-item>
-          <el-form-item class="el-form-item">
-            <el-button type="primary" @click="pageSearchEvent">查询</el-button>
-            <el-button @click="pageSearchResetEvent">重置</el-button>
-          </el-form-item>
-          <el-form-item class="el-form-year">
-            <span
-              @click="searchYearEvent(1)"
-              :class="this.searchFormData.searchDateType == 1 ? 'active' : ''"
-              >日</span
-            >
-            <span
-              @click="searchYearEvent(2)"
-              :class="this.searchFormData.searchDateType == 2 ? 'active' : ''"
-              >月</span
-            >
-            <span
-              @click="searchYearEvent(3)"
-              :class="this.searchFormData.searchDateType == 3 ? 'active' : ''"
-              >年</span
-            >
-            <span
-              @click="searchYearEvent(4)"
-              :class="this.searchFormData.searchDateType == 4 ? 'active' : ''"
-              >总</span
-            >
-          </el-form-item>
-        </el-form>
-      </div>
-
-      <div class="app-table app-table-nowrap">
-        <el-table
-          :data="dataList"
-          border
-          stripe
-          :default-sort="{ prop: 'create_time' }"
-        >
-          <el-table-column label="序号" width="80px">
-            <template slot-scope="scope">{{
-              scope.$index + (page_current - 1) * page_size + 1
-            }}</template>
-          </el-table-column>
-          <el-table-column prop="number" label="物品编号"></el-table-column>
-          <el-table-column
-            prop="product_name"
-            label="物品名称"
-          ></el-table-column>
-          <el-table-column prop="unit" label="物品规格"></el-table-column>
-          <el-table-column prop="num" label="出库数量"></el-table-column>
-          <el-table-column prop="store" label="仓库名称"></el-table-column>
-          <el-table-column prop="store_type" label="仓库类型"></el-table-column>
-          <el-table-column
-            width="160"
-            prop="create_time"
-            label="出库时间"
-            sortable
-          ></el-table-column>
-        </el-table>
-        <div class="app-pagination" v-if="dataList.length !== 0">
-          <el-pagination
-            background
-            prev-text="上一页"
-            next-text="下一页"
-            layout="total,  prev, pager, next, jumper"
-            :current-page="this.page_current"
-            :page-size="this.page_size"
-            :total="this.page_total"
-            @current-change="pageChange"
+    <div class="app-page-select">
+      <el-form :inline="true">
+        <el-form-item class="el-form-item" label="物品名称：">
+          <el-input
+            maxlength="30"
+            v-model="searchFormData.searchName"
+            class="input-with-select"
+          ></el-input>
+        </el-form-item>
+        <el-form-item class="el-form-item" label="仓库类型：">
+          <el-select
+            v-model="searchFormData.searchStoreType"
+            placeholder="选择仓库类型"
           >
-          </el-pagination>
-        </div>
+            <el-option label="全部类型" value=""></el-option>
+            <el-option
+              v-for="item in storeTypeList"
+              :key="item.id"
+              :label="item.name"
+              :value="item.id"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item class="el-form-item" label="仓库名称：">
+          <el-select
+            v-model="searchFormData.searchStore"
+            placeholder="选择仓库名称"
+          >
+            <el-option label="全部仓库" value=""></el-option>
+            <el-option
+              v-for="item in storeList"
+              :key="item.id"
+              :label="item.name"
+              :value="item.id"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item class="el-form-item" label="出库时间：">
+          <el-date-picker
+            v-model="searchFormData.serachTime"
+            type="date"
+            placeholder="选择日期"
+          >
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item class="el-form-item">
+          <el-button type="primary" @click="pageSearchEvent">查询</el-button>
+          <el-button @click="pageSearchResetEvent">重置</el-button>
+        </el-form-item>
+        <el-form-item class="el-form-year">
+          <span
+            @click="searchYearEvent(1)"
+            :class="this.searchFormData.searchDateType == 1 ? 'active' : ''"
+            >日</span
+          >
+          <span
+            @click="searchYearEvent(2)"
+            :class="this.searchFormData.searchDateType == 2 ? 'active' : ''"
+            >月</span
+          >
+          <span
+            @click="searchYearEvent(3)"
+            :class="this.searchFormData.searchDateType == 3 ? 'active' : ''"
+            >年</span
+          >
+          <span
+            @click="searchYearEvent(4)"
+            :class="this.searchFormData.searchDateType == 4 ? 'active' : ''"
+            >总</span
+          >
+        </el-form-item>
+      </el-form>
+    </div>
+
+    <div class="app-table app-table-nowrap">
+      <el-table
+        :data="dataList"
+        border
+        stripe
+        :default-sort="{ prop: 'create_time' }"
+      >
+        <el-table-column label="序号" width="80px">
+          <template slot-scope="scope">{{
+            scope.$index + (page_current - 1) * page_size + 1
+          }}</template>
+        </el-table-column>
+        <el-table-column prop="number" label="物品编号"></el-table-column>
+        <el-table-column prop="product_name" label="物品名称"></el-table-column>
+        <el-table-column prop="num" label="出库数量"></el-table-column>
+        <el-table-column prop="unit" label="物品规格"></el-table-column>
+        <el-table-column prop="store" label="仓库名称"></el-table-column>
+        <el-table-column prop="store_type" label="仓库类型"></el-table-column>
+        <el-table-column
+          width="160"
+          prop="create_time"
+          label="出库时间"
+          sortable
+        ></el-table-column>
+      </el-table>
+      <div class="app-pagination" v-if="dataList.length !== 0">
+        <el-pagination
+          background
+          prev-text="上一页"
+          next-text="下一页"
+          layout="total,  prev, pager, next, jumper"
+          :current-page="this.page_current"
+          :page-size="this.page_size"
+          :total="this.page_total"
+          @current-change="pageChange"
+        >
+        </el-pagination>
       </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      searchFormData: { searchStore: "", searchDateType: 1 },
+      searchFormData: { searchStoreType:"",searchStore: "", searchDateType: 1 },
       page_current: 1,
       page_size: 20,
       page_total: 0,
@@ -182,6 +179,7 @@ export default {
     pageSearchResetEvent() {
       this.searchFormData = {
         searchStore: "",
+        searchStoreType:"",
         searchDateType: 1,
       };
       this.page_current = 1;
